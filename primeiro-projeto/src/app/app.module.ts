933fe96c7a2e058c7be3675cd7019915108411d3
jefferson-almeida <jeffersonrodrigoma@hotmail.com>
2019-01-25 18:43:17 -0200

    p4

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    MeuPrimeiro2Component,
+    InputPropertyComponent,
+    OutputPropertyComponent,
+    CicloComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    CursosModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- primeiro-projeto/src/app/meu-primeiro2/meu-primeiro2.component.ts
+++ primeiro-projeto/src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -7,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeuPrimeiro2Component implements OnInit {
 
+  url: string = 'www.databinding.com';
+  cursoAngular = true;
+  urlImagem = 'http://lorempixel.com/400/200/city';
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver:boolean = false;
+
+  nome:string = 'abc';
+
+  nomeDoCurso: string = 'Angular'
+
+  pessoa: any = {
+    nome: 'def',
+    idade: 20
+  }
+
+  getCurtirCurso(){
+    return false;
+  }
+
+  getValor(){
+    return 4;
+  }
+
+  botaoClicado(){
+    alert('botao clicado')
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual =(<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento){
+    console.log(evento.novoValor);
+  }
+
   constructor() { }
 
   ngOnInit() {
