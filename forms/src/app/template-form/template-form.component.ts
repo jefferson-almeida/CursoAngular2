647bb396842d30e90a469dca69464e3393dd5295
jeffersonrma <jeffersonrodrigom@gmail.com>
2019-04-15 20:20:41 -0300

    85

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -8,17 +10,53 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: '',
-    email: ''
+    nome: null,
+    email: null,
+    endereco: {
+      cep: null,
+      numero: null,
+      complemento: null,
+      rua: null,
+      bairro: null,
+      cidade: null,
+      estado: null
+    }
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  onSubmit(f){
-    console.log(f.value)
+  consultaCep(campo) {
+    this.usuario.endereco.rua = null;
+    this.usuario.endereco.bairro = null;
+    this.usuario.endereco.estado = null;
+    this.usuario.endereco.cidade = null;
+    console.log(campo);
+
+    if (campo.valid) {
+      this.http.get(`https://viacep.com.br/ws/${campo.model}/json`)
+        .subscribe(dados => {
+          console.log(dados);
+          if(dados["erro"]) campo.control.setErrors({'incorrect': true});
+          this.usuario.endereco.rua = dados["logradouro"];
+          this.usuario.endereco.bairro = dados["bairro"];
+          this.usuario.endereco.estado = dados["uf"];
+          this.usuario.endereco.cidade = dados["localidade"];
+        });
+    }
+  }
+
+  onSubmit(f) {
+    console.log(f.value);
   }
 
   ngOnInit() {
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'is-invalid': campo.invalid && campo.dirty,
+      'is-valid': campo.valid && campo.touched
+    }
   }
 
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { TemplateFormComponent } from './template-form.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
 
 @NgModule({
   declarations: [
-    TemplateFormComponent
+    TemplateFormComponent,
+    FormDebugComponent,
+    CampoControlErroComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class TemplateFormModule { }
