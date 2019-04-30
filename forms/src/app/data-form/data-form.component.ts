e387c2416188803763f276eae7bbc7a9415fdbe3
jeffersonrma <jeffersonrodrigom@gmail.com>
2019-04-30 12:15:30 -0300

    100

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-data-form',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataFormComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+    ) { }
 
   ngOnInit() {
+    // this.formulario = new FormGroup({
+    //   nome: new FormControl('nome'),
+    //   email: new FormControl('e-mail')
+    // })
+
+    this.formulario = this.formBuilder.group({
+      nome: ['',[Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]]
+    });
   }
+
+  onSubmit(){
+    console.log(this.formulario);
+
+    this.http.post('https://httpbin.org/pos', JSON.stringify(this.formulario.value))
+      .subscribe(dados => {
+        console.log(dados);
+        this.resetarForm();
+      }, (error: any) => alert('um erro aconteceu' + error.message));
+
+  }
+  resetarForm() {
+    this.formulario.reset();
+  }
+
+  aplicaCssErro(campo) {
+    if(this.formulario.get(campo).dirty)
+      return this.formulario.get(campo).invalid  ? 'is-invalid' : 'is-valid'
+  }
+
+
 
 }
--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { DataFormComponent } from './data-form.component';
+import { SharedModule } from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     DataFormComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    SharedModule,
     CommonModule,
-    FormsModule
+    HttpClientModule
   ]
 })
 export class DataFormModule { }
